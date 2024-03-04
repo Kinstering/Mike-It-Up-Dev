5672b93e2889d803af5654ee38a836b7ae247c8f
Kinster <fantomchannel68@gmail.com>
2024-03-04 20:35:52 +0200

    Changing handlers

--- asks_handler.py
+++ asks_handler.py
@@ -26,26 +26,28 @@ api_secret = "YcOEDE19tnJIRZJxgI9hEugVmha4grCrEXDCJH7kNRJtdIwN38QO9FjFc71n636c"
 
 BINANCE_END_POINT = "https://api.binance.com/api/v3/userDataStream"
 BINANCE_FUTURES_END_POINT = "https://fapi.binance.com/fapi/v1/listenKey"
-real_time_price = {}
-# 'ONGUSDT', , 'IDUSDT'
-max_coin_v_1h = {}
 
 
 def create_spot_listen_key(api_key):
     response = requests.post(url=BINANCE_END_POINT, headers={'X-MBX-APIKEY': api_key})
     return response.json()['listenKey']
 
+def set_file_name(type_, number_):
+    current_time = datetime.datetime.now()
+    current_minute = current_time.minute
+    current_hour = current_time.hour
+    current_day = current_time.day
+    current_month = current_time.month
+    current_year = current_time.year
+
+    minute_interval = (current_minute // 15) * 15
+    fname = f"{type_}_{number_}_{current_year}_{current_month}_{current_day}_{current_hour}_{minute_interval}.csv"
+    return fname
+
 def on_message(ws, message, n):
     global was_time
     global total_a_df
-
     global total_b_df
-
-    global real_time_price
-
-    if os.path.exists(f'data/asks_{n}.csv')==False:        total_a_df     = total_a_df[0:0]
-
-    if os.path.exists(f'data/bids_{n}.csv')==False:        total_b_df     = total_b_df[0:0]
 
     response = json.loads(message)
     if message.find('depthUpdate') > 0:
@@ -56,25 +58,26 @@ def on_message(ws, message, n):
         df_a[["price", "v"]] = df_a[["price", "v"]].apply(pd.to_numeric)
         df_a['coin'] = response['s']
 
-        if (len(df_b) > 0):
+        if len(df_b) > 0:
             total_b_df = pd.concat([total_b_df, df_b])
 
-        if (len(df_a) > 0):
+        if len(df_a) > 0:
             total_a_df = pd.concat([total_a_df, df_a])
 
         now_time = int(int(time.time()) / 5) * 5 * 1000
         if was_time != now_time:
             was_time = now_time
-            total_a_df.to_csv(f"data/asks_{n}.csv", sep=',', mode='a', header=not os.path.exists(f"data/asks_{n}.csv"))
-            
-            total_b_df.to_csv(f'data/bids_{n}.csv', sep=',', mode='a', header=not os.path.exists(f"data/bids_{n}.csv"))
+            asks_name = set_file_name("asks", n)
+            bids_name = set_file_name("bids", n)
 
+            total_a_df.to_csv(f"data/{asks_name}", sep=',', mode='a', header=not os.path.exists(f"data/{asks_name}"))
+            total_b_df.to_csv(f"data/{bids_name}", sep=',', mode='a', header=not os.path.exists(f"data/bids{bids_name}"))
 
-            total_a_df     = total_a_df[0:0]
+            total_a_df = total_a_df[0:0]
+            total_b_df = total_b_df[0:0]
 
-            total_b_df     = total_b_df[0:0]
             print(f' - asks bids {n} OK')
-# --------------------------------------------
+
 def on_error(ws, error):
     print(error)
 
@@ -82,15 +85,12 @@ def on_close(close_msg):
     print("### closed ###" + close_msg)
 
 def main(n, array_count):
-    global real_time_price
     websocket.enableTrace(False)
     listen_key = create_spot_listen_key(api_key)
     mystreams = ''
     for cc_ in array_count:
         mystreams = mystreams + '/' + cc_.lower() + '@depth'
-    # 'solusdt@depth'
     socket = f'wss://data-stream.binance.com:9443/ws/{listen_key}' + mystreams
-    # print(socket)
     ws = websocket.WebSocketApp(socket, on_message=lambda ws, msg: on_message(ws, msg, n), on_error=on_error, on_close=on_close)
     ws.run_forever(dispatcher=rel, ping_interval=300, reconnect=20)
     rel.signal(2, rel.abort)  # Keyboard Interrupt
--- trades_handler.py
+++ trades_handler.py
@@ -6,52 +6,72 @@ import time
 import requests
 import rel
 import os
+from datetime import datetime, timedelta
 
 was_time = 0
-start_time=0
+total_df = pd.DataFrame()
+
+# ---------------------------------------
+def set_file_name(type_, number_):
+    current_time = datetime.now()
+    current_minute = current_time.minute
+    current_hour = current_time.hour
+    current_day = current_time.day
+    current_month = current_time.month
+    current_year = current_time.year
+
+    minute_interval = (current_minute // 15) * 15
+    fname = f"{type_}_{number_}_{current_year}_{current_month}_{current_day}_{current_hour}_{minute_interval}.csv"
+    return fname
+
 # ------------------------------------------
 def on_message(ws, message, n):
     global was_time
     global total_df
 
-    if os.path.exists(f'data/trades_{n}.csv') == False:     total_df     = total_df[0:0]
-
     response = json.loads(message)
     df = pd.json_normalize(response)
     df = df.rename(columns={"s": "coin", "p": "price", "q": "amount", "m": "buyer"})
     df = df.drop(['e', 'E', 'a', 'f', 'l', 'T', 'M'], axis=1)
-    total_df     = pd.concat([total_df, df])
 
-    now_time = int(int(time.time()) / 16) * 16 * 1000
+    total_df = pd.concat([total_df, df])
+
+    now_time = int(int(time.time()) / 900) * 900 * 1000
     if was_time != now_time:
         was_time = now_time
-        total_df.to_csv(f"data/trades_{n}.csv", sep=',', mode='a', header=not os.path.exists(f"data/trades_{n}.csv"))
-
-        total_df     = total_df[0:0]
+        fname = set_file_name("trades", n)
+        total_df.to_csv(f"data/{fname}", sep=',', mode='a', header=not os.path.exists(f"data/{fname}"))
+        total_df = total_df[0:0]
 
         print(f' - trades {n} OK')
+
 # ------------------------------------------
 def on_error(ws, error):
     print(error)
+
 # ------------------------------------------
 def on_close(close_msg):
     print("### closed ###" + close_msg)
+
 # ------------------------------------------
 def create_spot_listen_key(api_key):
     response = requests.post(url=end_point, headers={'X-MBX-APIKEY': api_key})
-    #print(response.json())
     return response.json()['listenKey']
+
 # ------------------------------------------
 def CreateStream(n, array_count):
     listen_key = create_spot_listen_key(api_key)
-    mystreams =''
+    mystreams = ''
     for cc_ in array_count:
         mystreams = mystreams + '/' + cc_.lower() + '@aggTrade'
+
     socket = f'wss://data-stream.binance.com:9443/ws/{listen_key}' + mystreams
-    #print(socket)
+
     ws = websocket.WebSocketApp(socket, on_message=lambda ws, msg: on_message(ws, msg, n), on_error=on_error, on_close=on_close)
     ws.run_forever(dispatcher=rel, ping_interval=300, reconnect=20)
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
+
 # --------------------------------------
 start_time = int(int(time.time()) / 16) * 16 * 1000
+
